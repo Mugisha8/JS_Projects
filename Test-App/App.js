7f36916fbedf259620d0e2c672779f569f3fe827
Mugisha8 <yvesmugisha8@gmail.com>
2024-01-03 22:18:16 +0200

    using emitter args

--- Test-App/App.js
+++ Test-App/App.js
@@ -41,7 +41,7 @@ const EventEmitter = require("events");
 
 const emitter = new EventEmitter();
 
-emitter.on("messageLogged", function () {
-  console.log("Listener called");
+emitter.on("messageLogged", (arg)=> {
+  console.log("Listener called",arg);
 });
-emitter.emit("messagelogged");
+emitter.emit("messagelogged",{id:1,url:"//https:meightGroupLtd.rw"});
