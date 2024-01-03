ef911bd4457bb97089a662ee21a4df38cb44d57f
Mugisha8 <yvesmugisha8@gmail.com>
2024-01-03 22:45:36 +0200

    server init

--- Test-App/App.js
+++ Test-App/App.js
@@ -45,3 +45,24 @@
 //   console.log("Listener called",arg);
 // });
 // emitter.emit("messagelogged",{id:1,url:"//https:meightGroupLtd.rw"});
+
+
+const http = require("http")
+
+const server = http.createServer((req,res)=>{
+
+if(req.url === "/")
+{
+    res.write("This Is Meight Industries");
+    res.end();
+}
+
+});
+
+
+server.listen(3000)
+
+console.log("Listening on port 3000")
+
+
+
