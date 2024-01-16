f28f55fc934b5b8db96a42b179ce3a0226d3ba4c
Mugisha8 <yvesmugisha8@gmail.com>
2024-01-16 23:07:23 +0200

    schema values

--- Models/ProductModel.js
+++ Models/ProductModel.js
@@ -1,12 +1,24 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const ProductSchema = mongoose.Schema({
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "please enter the name of the product"],
+    },
 
-name:{
-   type:String,
-   required:[true,"please enter the name of the product"] 
-}
+    quantity: {
+      type: Number,
+      required: true,
+    },
 
-
-
-})
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps:true
+  }
+);
