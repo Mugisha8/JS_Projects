9131f6d13e4a2fcc8198728f5892ac5f2872e790
Mugisha8 <yvesmugisha8@gmail.com>
2024-01-16 22:57:07 +0200

    Initializing routes

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const app = express();
+
+//routes
+
+app.get("/", (req, res) => {
+  res.send("Meight Rwanda ");
+});
+
+app.get("/stocks", (req, res) => {
+  res.send("meight stocks in Rwanda");
+});
