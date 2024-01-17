c2e4c83a48efd8043f128a77c3bbc7b39f1945a0
Mugisha8 <yvesmugisha8@gmail.com>
2024-01-17 22:46:28 +0200

    put Rest api init

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
 import express from "express";
-import apartments from "./Models/ProductModel";
+import apartments from "./Models/ProductModel.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //routes
 
@@ -22,6 +25,50 @@ app.post("/apartments", async (req, res) => {
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/apartments", async (req, res) => {
+  try {
+    const apartment = await apartment.find({});
+    res.status(200).json(apartment);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/apartment/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const apartment = await apartment.findbyId(id);
+    if (!apartment) {
+      res
+        .status(404)
+        .json({ message: `No apartment Available for this Id ${id}` });
+    }
+    res.status(200).json(apartment);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/apartments/:id", async (req, res) => {
+  try {
+
+    const {id}=req.params
+    const apartment = await apartments.findbyIdandUpdate(id)
+    if(!apartment){
+      res.status(404).json({message:`Apartment not updated`})
+    }
+    res.status(200).json({message:`apartment updated`});
+
+
+
+  } catch (error) {
+  console.log(error.message);
+  res.status(500).json({message:error.message})
   }
 });
 
