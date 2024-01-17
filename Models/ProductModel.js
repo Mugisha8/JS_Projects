91375becc863e1661a1a5a3034bc9a26d260c90d
Mugisha8 <yvesmugisha8@gmail.com>
2024-01-17 15:12:00 +0200

    schemas init

--- Models/ProductModel.js
+++ Models/ProductModel.js
@@ -1,3 +1,16 @@
 const mongoose = require("mongoose");
 
-const apartmentsSchema = mongoose.Schema({})
+const apartmentsSchema = mongoose.Schema({
+  name:{
+    type:String,
+    required:[true,"Please enter the name of the apartments"],
+  },
+  location:{
+    type:String,
+    required:[true,"Enter the Location of the apartment"]
+  },
+  price:{
+  type:String,
+  required:true
+  }
+})
