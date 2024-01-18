4aff19528e4d93a36dde23611f5ada91eaaa0851
Mugisha8 <yvesmugisha8@gmail.com>
2024-01-18 21:40:40 +0200

    api crud

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import express from "express";
 import apartmentRouter from "./routes/apartmentRoute.js";
-
+import { errorMiddleware } from "./middleware/errorMiddleware.js";
+import cors from "cors";
 //end of imports
 
 const app = express();
@@ -11,19 +12,38 @@ const app = express();
 
 dotenv.config();
 const MONGO_URL = process.env.MONGO_URL;
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 //end of env configuration
 
+//-------- cross origin conf
+
+// const corsOptions = {
+//   origin:"www.example.com",
+//   optionsSucessStatus :200
+// }
+
+//-------- cross origin conf
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // routing
 
-app.use("/meight",apartmentRouter)
+app.get("/", (req, res) => {
+  res.send("MEIGHT Rwanda");
+});
+
+app.use("/meight", apartmentRouter);
 
 // end of routing
 
+//start of Middleware conf
+
+app.use(errorMiddleware);
+
+//end of Middleware conf
 
 //db connection
 
